25232b23f4a7502fc28a249c2af1bd8af972c478
ChrisRaven <kr.kruk@gmail.com>
2024-10-29 19:59:36 +0100

    Basic setup

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+from functionalities.get_synaptic_partners import get_synaptic_partners
+# import get_all_annotations
+
+from tkinter import *
+top = Tk()
+top.geometry('1280x800')
+# Code to add widgets will go here...
+
+def helloCallBack():
+    a = 5
+
+B = Button(top, text ="Hello", command = helloCallBack)
+B.place(x=50,y=50)
+
+top.mainloop()
+
+get_synaptic_partners([720575941536100318,720575941338817903,720575941605725549])
